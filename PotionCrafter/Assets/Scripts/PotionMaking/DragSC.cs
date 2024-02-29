60644b0bd8fb8e5b8ceae361661cee8d88f8297c
DolonStoned <148004072+DolonStoned@users.noreply.github.com>
2024-02-29 13:45:20 +0000

    Started Potion Making
    Made a draggable object for use in the cutting minigame

---
+++ PotionCrafter/Assets/Scripts/PotionMaking/DragSC.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Dragable : MonoBehaviour
+{
+    public float damping;
+    public float frequency;
+    public LayerMask draglayer;
+    private TargetJoint2D joint;
+
+    private void Update()
+    {
+        var worldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            var draggingobjectcollider = Physics2D.OverlapPoint(worldpos, draglayer);
+            if (!draggingobjectcollider)
+            {
+                return;
+            }
+            var dragginobjectbody = draggingobjectcollider.attachedRigidbody;
+            if (!dragginobjectbody)
+            {
+                return;
+            }
+            joint = dragginobjectbody.gameObject.AddComponent<TargetJoint2D>();
+            joint.dampingRatio = damping;
+            joint.frequency = frequency;
+            joint.anchor = joint.transform.InverseTransformPoint(worldpos);
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(joint);
+            joint = null;
+        }
+
+
+        if (joint)
+        {
+            joint.target = worldpos;
+        }
+    }
+}
