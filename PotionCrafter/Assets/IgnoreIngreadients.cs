fca87b27e10ca38e060d95c3e61d3d9450217182
DolonStoned <148004072+DolonStoned@users.noreply.github.com>
2024-03-05 22:40:22 +0000

    .

---
+++ PotionCrafter/Assets/IgnoreIngreadients.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreIngreadients : MonoBehaviour
+{
+    public Collider2D thisCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ingredient"))
+        {
+            Physics2D.IgnoreCollision(thisCollider, collision.gameObject.GetComponent<Collider2D>());
+        }
+    }
+}
