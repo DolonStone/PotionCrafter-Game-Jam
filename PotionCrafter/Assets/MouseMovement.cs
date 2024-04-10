7cb79b8c23d0c0f19f7d217cb2a4b9657840fd12
DolonStoned <148004072+DolonStoned@users.noreply.github.com>
2024-04-10 14:27:37 +0100

    trail for knife

--- PotionCrafter/Assets/MouseMovement.cs
+++ PotionCrafter/Assets/MouseMovement.cs
@@ -8,6 +8,7 @@ public class MouseMovement : MonoBehaviour
     public Vector3 mouseFrameMovement = Vector3.zero;
     private Vector3 lastMousePos = Vector3.zero;
     private int posBuffer = 0;
+    public GameObject thingToFollowPos;
     void Update()
     {
         mouseFrameMovement = Input.mousePosition - lastMousePos;
@@ -17,5 +18,19 @@ public class MouseMovement : MonoBehaviour
 
         }
         posBuffer += 1;
+        if (thingToFollowPos != null)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            thingToFollowPos.transform.position = new Vector3(cursorPos.x,cursorPos.y,3);
+            if (mouseFrameMovement.magnitude >= 25 && Input.GetMouseButton(0))
+            {
+                thingToFollowPos.SetActive(true);
+            }
+            else
+            {
+                thingToFollowPos.SetActive(false);
+            }
+        }
+
     }
 }
